1645ef78743f669ac3b970b25719aecc744d6c58
Jongbin Jung <jongbin@stanford.edu>
2017-02-04 21:05:28 -0800

    Setup deploy script

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+git log --pretty=format:"%s" --since="$(cat last_deploy)" > daddy.jongbin.com/upstream_commits
+date +"%F %T" > last_deploy
+
+# Build the project.
+hugo -d daddy.jongbin.com
+
+# Go To Public folder
+cd daddy.jongbin.com
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="Rebuild site `date +'%F %T'`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg" -m "$(cat upstream_commits)"
+
+# Push source and build repos.
+git push origin master
+
+# Come Back
+cd ..
+
+
+# Older rsync-based one-liner
+# rsync -rlpdoDz --force --delete --progress -e "ssh -p22" public/ jongbin@jongbin.com:/home/jongbin/www/blog
